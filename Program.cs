9a423ff7e9f7e6785ace872d3c4afe18e18d9c56
Askold Makaruk <askoldmakaruk@gmail.com>
2020-01-29 14:26:45 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using BotFramework.Bot;
+using Serilog;
+
+namespace BFTemplate
+{
+    class Program
+    {
+        static void Main()
+        {
+            new BotBuilder()
+            .UseAssembly(typeof(Program).Assembly)
+            .WithName("EchoBot")
+            .WithToken("<YOURTOKEN>")
+            .UseLogger(new LoggerConfiguration()
+                       .MinimumLevel.Debug()
+                       .WriteTo.Console()
+                       .CreateLogger())
+            .Build()
+            .Run();
+        }
+    }
+}
