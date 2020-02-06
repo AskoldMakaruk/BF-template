336c16eb3995ec89f521b829004edbb19bf52992
Askold Makaruk <askoldmakaruk@gmail.com>
2020-02-06 16:52:58 +0200

    some comments

--- Program.cs
+++ Program.cs
@@ -20,3 +20,19 @@ namespace BFTemplate
         }
     }
 }
+/*
+ * amazing guide about webhooks
+ * https://core.telegram.org/bots/webhooks
+ *
+ * Useful commands:
+ * -Efcore:
+ * -- dotnet ef migrations add initial
+ * -- dotnet ef database update
+ *
+ * Packages
+ * dotnet add package Microsoft.EntityFrameworkCore.Tools
+ * dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
+ * dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL.Design
+ *
+ *  
+ */
